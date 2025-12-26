95c909d58fdd9bdf3c773ebac047e0f122306103
Manaf <manaf18.m@gmail.com>
2025-12-26 19:27:30 +0000

    implement structured streaming pipeline with Postgres sink

--- streaming_pipeline.py
+++ streaming_pipeline.py
@@ -1,13 +1,14 @@
 from pyspark.sql import SparkSession
 
 from etl.extractor import read_events
+from etl.load import load_to_postgres
 from etl.transform import transform_data
 
 
 def initiate_spark():
     spark = (
         SparkSession.builder.appName("SparkStreaming")
-        .config("spark.jars", "/streaming/spark/jars")
+        .config("spark.jars", "/streaming/spark/jars/postgresql-42.7.6.jar")
         .config(
             "spark.sql.streaming.checkpointLocation", "/streaming/spark/checkpoints"
         )
@@ -19,9 +20,28 @@ def initiate_spark():
 
 
 def main():
+    print("Starting ETL process...")
     spark = initiate_spark()
-    stream_df = read_events(spark)
-    transformed_df = transform_data(stream_df)
+    try:
+        stream_df = read_events(spark)
+        transformed_df = transform_data(stream_df)
+        streaming_query = (
+            transformed_df.writeStream.foreachBatch(load_to_postgres)
+            .outputMode("append")
+            .trigger(processingTime="10 seconds")
+            .start()
+        )
+        streaming_query.awaitTermination()
+
+    except KeyboardInterrupt:
+        print("Exiting ETL process...")
+        for query in spark.streams.active:
+            query.stop()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        spark.stop()
+        print("ETL process completed.")
 
 
 if __name__ == "__main__":
